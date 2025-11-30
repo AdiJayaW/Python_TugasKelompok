af23ca1a743b33f86df815cd88fabb1c8194ddba
Adi Jaya Wibawa <137131870+AdiJayaW@users.noreply.github.com>
2025-11-30 08:41:32 +0700

    update_countersspam

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import time
 balance = 0
+counter_spam = 0
+max_spam = 3
+
 
 def main():
+    global counter_spam
     print(""" 
     ========================================|
     Selamat Datang di Simulasi ATM SEDERHANA|
-           Bersama dengan Adi dan Jefry     |
     ========================================|
     
 
@@ -18,23 +21,46 @@ def main():
 3. Tarik Tunai
 4. Keluar
 """)
-        choice = int(input("Pilih opsi (1-4): "))
+        try:
+            choice = input("Pilih opsi (1-4): ")
+            choice_convert = int(choice)
 
-        # Fungsi untuk Memilih Opsi dari Menu yang tersedia
+            # Mendeklarasikan agar opsi 1 dan 4 mereset counter spam menjadi 0, agar tidak dihitung kemudian menjadi 1 lagi.
+            if 1<= choice_convert <= 4:
+                counter_spam = 0
 
-        if choice == 1:
-            check_balance()
-        elif choice == 2:
-            amount = int(input("Masukkan jumlah yang akan disetorkan dalam bentuk rupiah: "))
-            deposit(amount)
-        elif choice == 3:
-            amount = int(input("Masukkan jumlah yang akan ditarik dalam bentuk rupiah: "))
-            withdraw(amount)
-        elif choice == 4:
-            print("Terima kasih telah menggunakan Layanan ATM Sederhana, Adios Mabroo!")
+                if choice_convert == 1: #jika pilih angka "1", maka akan masuk ke fungsi check_balance
+                    check_balance()
+                elif choice_convert == 2: #jika pilih angka "2", maka bisa memasukkan nilai ke variabel amount, lalu nilai tersebut dibuat parameter, ketika fungsi deposit dijalankan.
+                    amount = int(input("Masukkan jumlah yang akan disetorkan dalam bentuk rupiah: "))
+                    deposit(amount)
+                elif choice_convert == 3:
+                    amount = int(input("Masukkan jumlah yang akan ditarik dalam bentuk rupiah: "))
+                    withdraw(amount)
+                elif choice_convert == 4:
+                    print("Terima kasih telah menggunakan Layanan ATM Sederhana, Adios Mabroo!")
+            else :
+                print("Pilihan tidak valid. Silahkan coba lagi.")
+                counter_spam += 1
+                print(f"Peringatan Spam {counter_spam}/{max_spam}")
+                time.sleep (0.8)
+
+            if counter_spam >= max_spam:
+                print (f"Anda telah melakukan percobaan {max_spam} kali, coba ulangi beberapa saat lagi")
+                break
+
+        except ValueError :
+            print("❌ Input tidak valid. Kamu harus memasukkan angka.")
+            counter_spam += 1
+            print(f"Peringatan Spam: {counter_spam}/{max_spam}")
+
+            if counter_spam >= max_spam:
+                print (f"Anda telah melakukan percobaan {max_spam} kali, coba ulangi beberapa saat lagi")    
+                break    
+
+        except Exception as e:
+            print(f"⚠️ Terjadi kesalahan tak terduga: {e}")
             break
-        else:
-            print("Pilihan tidak valid. Silahkan coba lagi.")
 
 def check_balance():
     print(f"Saldo Anda saat ini adalah: {balance}")
